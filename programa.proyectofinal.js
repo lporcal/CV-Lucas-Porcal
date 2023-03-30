0b42f69bf2b4ec6e09a6b2bccb250666c8462228
Lucas Porcal <lucas_porcal@hotmail.com>
2023-03-29 23:34:23 -0300

    Correcciones finales para segunda entrega del Proyecto

--- programa.proyectofinal.js
+++ programa.proyectofinal.js
@@ -16,38 +16,6 @@ const i5 = 'Intereses'
 const i6 = 'Referencias'
 let items = [i1, i2, i3, i4, i5, i6]
 
-const t1 = 'T1'
-const t2 = 'T2'
-const t3 = 'T3'
-const t4 = 'T4'
-const t5 = 'T5'
-let trabajos = [t1, t2, t3, t4, t5]
-
-const div1 = 'div 1'
-const div2 = 'div 2'
-const div3 = 'div 3'
-const div4 = 'div 4'
-const div5 = 'div 5'
-let divs = [div1, div2, div3, div4, div5]
-
-const bc1 = 'C1'
-const bc2 = 'C2'
-const bc3 = 'C3'
-const bc4 = 'C4'
-const bc5 = 'C5'
-const bc6 = 'C6'
-const bc7 = 'C7'
-let botoncap = [bc1, bc2, bc3, bc4, bc5, bc6, bc7]
-
-const c1 = 'Cap 1'
-const c2 = 'Cap 2'
-const c3 = 'Cap 3'
-const c4 = 'Cap 4'
-const c5 = 'Cap 5'
-const c6 = 'Cap 6'
-const c7 = 'Cap 7'
-let capacitaciones = [c1, c2, c3, c4, c5, c6, c7]
-
 // Recorre cada item y le indica que inicialmente debe estar la solapa cerrada
 items.forEach(function (item) {
     document.getElementById(item).style.display = 'none';
@@ -84,66 +52,3 @@ function abririnfo(boton) {
 function ventanaAuxiliar(ubicacion) {
     window.open(ubicacion,"Detalle de información", "width= 1000, height= 1500, scrollbars= NO")
 }
-
-//Recorre cada boton de los trabajos y cuando se le hace click, oculta todos los div
-//Deja visible el div asociado al trabajo que quiero mostrar
-
-trabajos.forEach(function (trabajo) {
-    document.getElementById(trabajo).addEventListener('click', function () { 
-            ocultartodoslosdivs();
-            mostrardiv(trabajo);
-    });
- })
-
-//Funcion ocultar todos los divs
-function ocultartodoslosdivs(){
-    divs.forEach(function (divn) {    
-        document.getElementById(divn).classList.remove ("visible");
-        document.getElementById(divn).classList.add ("oculto");
-    });
-}
-
-//Funcion mostrar todos los divs
-function mostrartodoslosdivs(){
-    divs.forEach(function (divn) {    
-        document.getElementById(divn).classList.remove ("oculto");
-        document.getElementById(divn).classList.add ("visible");
-    });
-}
-
-//Funcion mostrar div asociado al trabajo seleccionado
-function mostrardiv(trabajo){
-        document.getElementById(divs[trabajos.indexOf(trabajo)]).classList.remove ("oculto")
-        document.getElementById(divs[trabajos.indexOf(trabajo)]).classList.add ("visible");
-}
-
-
-//Recorre cada botoncap y cuando se le hace click, oculta todos los divs capacitaciones
-//Deja visible el div capacitacion asociada a la capacitacion que quiero mostrar
-
-botoncap.forEach(function(bcn) {
-    document.getElementById(bcn).addEventListener('click', function () {
-            ocultartodaslascapacitaciones();
-            mostrarcapacitacion(bcn);
-    });
-})
-
-function ocultartodaslascapacitaciones(){
-        capacitaciones.forEach(function(capacitacion) {
-            document.getElementById(capacitacion).classList.remove ("visible");
-            document.getElementById(capacitacion).classList.add ("oculto");
-        });
-    }
-
-function mostrartodaslascapacitaciones(){
-        capacitaciones.forEach(function(capacitacion) {
-            document.getElementById(capacitacion).classList.remove ("oculto");
-            document.getElementById(capacitacion).classList.add ("visible");
-        });
-    }
-    
-function mostrarcapacitacion(bcn){
-            document.getElementById(capacitaciones[botoncap.indexOf(bcn)]).classList.remove ("oculto");
-            document.getElementById(capacitaciones[botoncap.indexOf(bcn)]).classList.add ("visible");
-    }
-
